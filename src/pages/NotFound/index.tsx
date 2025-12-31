a979dd2b121b1adac41718a5d56123cde7ae4f58
Talyslan <talyslancpc@gmail.com>
2025-12-31 01:45:05 -0300

    feat: stylization added

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -12,8 +12,8 @@ export default function NotFound() {
     }, [location.pathname]);
 
     return (
-        <div className="bg-muted flex min-h-screen items-center justify-center">
-            <div className="text-center">
+        <div className="bg-background flex min-h-screen items-center justify-center">
+            <div className="space-y-4 text-center">
                 <h1 className="mb-4 text-4xl font-bold">404</h1>
                 <p className="text-muted-foreground mb-4 text-xl">
                     Oops! Page not found
