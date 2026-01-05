5076fb7dcfb76719f49d9c68f429c0a783ce18fe
Talyslan <talyslancpc@gmail.com>
2026-01-05 01:44:56 -0300

    fix: remove env variable about email 'resend'

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,6 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
     VITE_GITHUB_TOKEN: z.string(),
-    VITE_RESEND_API_KEY: z.string(),
 });
 
 export const env = envSchema.parse(import.meta.env);
